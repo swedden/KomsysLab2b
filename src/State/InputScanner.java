90ec184130f091292cdce82228d47fd52b3af8a8
Fredrik <fberntss@kth.se>
2018-10-01 23:58:40 +0200

    input check for right classes at the right time (all files..)

---
+++ src/State/InputScanner.java
@@ -0,0 +1,73 @@
+package State;
+
+import java.util.Scanner;
+
+/**
+ * Pga att man i olika delar av programmet vill skriva in från System.in och programmet använder trådar kan man inte ha
+ * scanners på alla ställen då dessa krockar med varnnadra (med System.in). Denna klassen sköter det så endas och endast
+ * en i taget kan läsa in från System.in (scanner). Klassen fungerar så att den läser in allt från användaren, och för
+ * att skilja åt vem som ska ha meddelandet används classString.
+ */
+
+public class InputScanner
+{
+    private Scanner scanner;
+    private String classString;
+    private String inputMessage;
+    public InputScanner()
+    {
+        scanner = new Scanner(System.in);
+        inputMessage = "";
+        Thread inputThread = new Thread()
+        {
+            @Override
+            public void run()
+            {
+                while(true)
+                {
+                    inputMessage = scanner.nextLine().toUpperCase();
+                }
+            }
+        };
+        inputThread.start();
+    }
+
+    //overrids old classString so that a new class can get the inputMessage
+    public void setClassString(String classString)
+    {
+        this.classString = classString;
+    }
+
+    //returns inputmessage if this.classString classString
+    public String getUserInput(String classString)
+    {
+        if(this.classString.equals(classString))
+        {
+            String tmp = inputMessage;
+            inputMessage = "";
+            return tmp;
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    //returns true if there is a new message from user (System.in) and is for the right class
+    public boolean hasInput(String classString)
+    {
+        if(!(inputMessage.length() == 0))
+        {
+            System.out.println("inputMessage.length() == " + inputMessage.length() + ", inputmessage: " + inputMessage + ", classString: " + classString);
+            if(this.classString.equals(classString))
+            {
+                return true;
+            }
+            return false;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
